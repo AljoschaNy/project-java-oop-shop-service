fb62c4a3015f0c30d74d940f2a941093994a1bff
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-11 12:55:24 +0200

    Change Product to Optional in getProductById method

--- src/main/java/ProductRepo.java
+++ src/main/java/ProductRepo.java
@@ -1,24 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepo {
     private List<Product> products;
 
     public ProductRepo() {
         products = new ArrayList<>();
+        products.add(new Product("1", "Apfel"));
     }
 
     public List<Product> getProducts() {
         return products;
     }
 
-    public Product getProductById(String id) {
+    public Optional<Product> getProductById(String id) {
         for (Product product : products) {
             if (product.id().equals(id)) {
-                return product;
+                return Optional.of(product);
             }
         }
-        return null;
+        return Optional.empty();
     }
 
     public Product addProduct(Product newProduct) {

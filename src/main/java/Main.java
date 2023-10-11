ab8a81b4debf2d567e3586d60bc6c8635722635a
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-11 17:47:21 +0200

    Add instances of ProductRepo, OrderRepo and ShopService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,40 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        ProductRepo products = new ProductRepo();
+        OrderRepo orders = new OrderMapRepo();
+
+        products.addProduct(new Product("p1","iPhone"));
+        products.addProduct(new Product("p2","Galaxy Phone"));
+        products.addProduct(new Product("p3","iPad"));
+        products.addProduct(new Product("p4","Mac Book"));
+        products.addProduct(new Product("p5","Xiaomi Phone"));
+
+        ShopService service = new ShopService(products,orders,0);
+
+        service.addOrder(List.of(
+                products.getProducts().get(0).id(),
+                products.getProducts().get(1).id(),
+                products.getProducts().get(2).id())
+        );
+
+        service.addOrder(List.of(
+                products.getProducts().get(0).id(),
+                products.getProducts().get(1).id(),
+                products.getProducts().get(2).id())
+        );
+        service.addOrder(List.of(
+                products.getProducts().get(0).id(),
+                products.getProducts().get(1).id(),
+                products.getProducts().get(2).id())
+        );
+
+        service.addOrder(List.of("p2"));
+
+        System.out.println(service.getAllOrdersByOrderStatus(OrderStatus.PROCESSING).size());
+        service.updateOrder("3",OrderStatus.IN_DELIVERY);
+
+        System.out.println(service.getProductRepo());
+        System.out.println(service.getOrderRepo().getOrders());
     }
 }

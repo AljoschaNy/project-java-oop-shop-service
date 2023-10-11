3022ba7b776e8311d1afdc9dc48c4a69b2682418
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-11 11:36:45 +0200

    Add getAllOrdersByOrderStatus method

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
@@ -17,8 +18,14 @@ public class ShopService {
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products,OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
+    }
+
+    public List<Order> getAllOrdersByOrderStatus(OrderStatus orderStatus){
+         return orderRepo.getOrders().stream()
+                .filter(order -> order.orderStatus().equals(orderStatus))
+                .collect(Collectors.toList());
     }
 }
